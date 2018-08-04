f365f8faa45e3d336cb580ce3b7aa6d556e9dc82
wind666 <41415622+wind666@users.noreply.github.com>
2018-08-04 19:08:27 +0800

    Create Video_Capture.py

---
+++ Video_Capture.py
@@ -0,0 +1,30 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+#set the size of the window
+cap.set(3,620)
+cap.set(4,480)
+#write the video as the name:output.avi
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output.avi',fourcc,20.0,(640,480))
+
+while(True):
+# Capture frame-by-frame
+    ret, frame = cap.read()
+    #wrie video
+    #rotate 180--0,no rotate--other integer
+    frame=cv2.flip(frame,2)
+    out.write(frame)
+
+    # Our operations on the frame come here
+    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Display the resulting frame
+    cv2.imshow('frame',gray)
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        break
+
+    # When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
